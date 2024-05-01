4dd8171c882ad22a113e8c47adb5a1285d6f30c1
Haley Hodges <hhodges10@msn.com>
2024-05-01 18:58:53 -0500

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <h1>Testing out storybook for creating component library</h1>;
+  return <h1>Testing out storybook for creating component library and testing github branch rule</h1>;
 }
 
 export default App;
