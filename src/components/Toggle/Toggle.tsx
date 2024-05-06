a95720fd9d4a1b5dd3169830b4f0d827559fea2b
Haley Hodges <hhodges10@msn.com>
2024-05-05 21:08:14 -0500

    adding size option for toggle

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -5,6 +5,7 @@ export type ToggleProps = {
   label: string;
   withIconIndicator?: boolean;
   disabled?: boolean;
+  size?: 'sm' | 'md' | 'lg';
   className?: string;
   props?: any[];
   onChange: (event: any) => void;
@@ -25,28 +26,29 @@ const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
   width: 1px;
 `;
 
-const Icon = styled.svg`
-  ${({ theme: { color } }) => css`
+const Icon = styled.svg<{ $size: string }>`
+  ${({ theme: { color }, $size }) => css`
     fill: ${color.dark};
-    width: 15px;
-    height: 15px;
+    width: ${$size === 'md' ? '15px' : $size === 'sm' ? '10px' : '21px'};
+    height: ${$size === 'md' ? '15px' : $size === 'sm' ? '10px' : '21px'};
   `}
 `;
 
-const ToggleIndicator = styled.div<{ $checked: boolean }>`
-  ${({ theme: { color }, $checked }) => css`
+const ToggleIndicator = styled.div<{ $checked: boolean; $size: string }>`
+  ${({ theme: { color }, $checked, $size }) => css`
     position: absolute;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
     top: 3px;
     left: 3px;
-    height: 18px;
-    width: 18px;
+    height: ${$size === 'md' ? '18px' : $size === 'sm' ? '14px' : '24px'};
+    width: ${$size === 'md' ? '18px' : $size === 'sm' ? '14px' : '24px'};
     border-radius: 2px;
     background-color: ${color.lightVar};
     box-shadow: rgba(23, 25, 38, 0.3) 0px 0px 2px 2px;
 
     ${Icon} {
-      margin-top: 1.5px;
-      margin-left: 1.5px;
       animation: ${$checked ? '325ms fadeIn' : 'none'};
       animation-fill-mode: forwards;
       visibility: hidden;
@@ -67,18 +69,22 @@ const ToggleIndicator = styled.div<{ $checked: boolean }>`
   `}
 `;
 
-const ToggleBackground = styled.div<{ $checked: boolean; $disabled: boolean }>`
-  ${({ theme: { font, color }, $disabled, $checked }) => css`
+const ToggleBackground = styled.div<{
+  $checked: boolean;
+  $disabled: boolean;
+  $size: string;
+}>`
+  ${({ theme: { font, color }, $disabled, $checked, $size }) => css`
     position: relative;
-    height: 24px;
-    width: 46px;
+    height: ${$size === 'md' ? '24px' : $size === 'sm' ? '20px' : '30px'};
+    width: ${$size === 'md' ? '46px' : $size === 'sm' ? '42px' : '60px'};
     border-radius: 4px;
     background-color: ${$checked ? color.accent : color.backgroundVar};
     font-family: ${font.family};
     opacity: ${$disabled ? 0.8 : 1};
 
     ${ToggleIndicator} {
-      left: ${$checked ? '25px' : '3px'};
+      left: ${!$checked ? '3px' : $size !== 'lg' ? '25px' : '33px'};
       transition: left 150ms linear;
     }
 
@@ -124,6 +130,7 @@ function Toggle({
   checked,
   label = 'Toggle Switch Label',
   disabled = false,
+  size = 'md',
   withIconIndicator: withIcon = true,
   className,
   onChange,
@@ -139,10 +146,18 @@ function Toggle({
             onChange={onChange}
             {...props}
           ></HiddenCheckbox>
-          <ToggleBackground $checked={checked} $disabled={disabled}>
-            <ToggleIndicator $checked={checked}>
+          <ToggleBackground
+            $checked={checked}
+            $disabled={disabled}
+            $size={size}
+          >
+            <ToggleIndicator $checked={checked} $size={size}>
               {withIcon && (
-                <Icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <Icon
+                  $size={size}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
                   <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
                 </Icon>
               )}
